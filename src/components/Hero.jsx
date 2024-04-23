70152373e998283227a4012eedece01d11049230
EaineDan <eaine.dan98@gmail.com>
2024-04-23 05:36:52 +0000

    Homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
+import display from '../assets/house-banner.png'
+import { FaLocationPin,  } from 'react-icons/fa6';
+import { FaSearch } from 'react-icons/fa';
 
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="bg-black">
+      <div className="flex  ">
+        <div className="flex-1 pt-20 px-10">
+          <div className="justify-center text-center text-white gap -4 leading-loose">
+            <div className="h-[6rem] w-[6rem] rounded-full absolute bg-yellow-600 right-[57%] top-[22%] -z-0 "></div>
+            <h1 className="text-4xl p-5 relative z-1">Expand your Horizon</h1>
+
+            <h2 className="p-3 text-xl">
+              Discover New Exciting, <br />
+              Affordable and Easy to live in Apartments
+            </h2>
+            <p className="p-3 text-lg">
+              Find a variety of housing units,
+              <br /> with ultra modern facilities <br /> to provide you with the
+              comfort you so deserve
+            </p>
+            <p
+              className="text-lg p-3
+            "
+            >
+              An ideal home for your dream vacation
+            </p>
+            <div className="bg-slate-100 p-3 rounded-lg flex items-center mx-28 my-12 w-[60%] overflow-hidden justify-between ">
+              <FaLocationPin className="text-blue-500 text-2xl m-3 " />
+              <input
+                type="text"
+                placeholder="Search....."
+                className="bg-transparent focus:outline-none  sm:w-64 justify-between text-black text-lg"
+              />
+              <FaSearch className="text-blue-500 text-2xl" />
+            </div>
+          </div>
+        </div>
+        <div className="flex-1">
+          <div className="p-20">
+            <img
+              src={display}
+              alt=""
+              className="w-[25rem] h-[27rem] overflow-hidden rounded-t-full border-2 border-blue-300 object-cover"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,27 +3,29 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <header className="text-white bg-slate-500">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <h1 className='text-lg'>NisaVille</h1>
+    <header className="text-white bg-black w-full top-0 left-0  cursor-pointer ">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-2">
+        <h1 className="text-lg text-gray-300">NisaVille...</h1>
 
-        <ul className="flex text-lg gap-4">
-          <Link >
-          <li className="hidden sm:inline text-slate-700  hover:underline hover:underline-offset-8 hover:text-rose-500">
+        <ul className="flex text-lg gap-7 py-2  items-center text-white">
+          <Link>
+            <li className="hidden sm:inline hover:underline hover:underline-offset-8 hover:text-blue-300">
               Home
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700  hover:underline hover:underline-offset-8 hover:text-rose-500">
+            <li className="hidden sm:inline hover:underline hover:underline-offset-8 hover:text-blue-300">
               About
             </li>
           </Link>
           <Link to="/residence">
-            <li className="hidden sm:inline text-slate-700  hover:underline hover:underline-offset-8 hover:text-rose-500">
+            <li className="hidden sm:inline   hover:underline hover:underline-offset-8 hover:text-blue-300">
               Residencies
             </li>
           </Link>
-          <button className='bg-white text-emerald-800 w-20 h-10 rounded-full hover:bg-blue-300 hover:text-white'>Login</button>
+          <button className="bg-white text-emerald-800 w-20 h-10 rounded-full hover:bg-blue-500 hover:text-white">
+            Login
+          </button>
         </ul>
       </div>
     </header>
